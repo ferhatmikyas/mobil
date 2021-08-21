71e8999e077a30ae4d89cfa6e082b4df53a0de80
Ferhat Mikyas <mikyasferhat@yandex.com>
2021-08-21 05:07:13 +0300

    Initial Commit

---
+++ Shared/uygulamaApp.swift
@@ -0,0 +1,17 @@
+//
+//  uygulamaApp.swift
+//  Shared
+//
+//  Created by Ferhat Mikyas on 21.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct uygulamaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
